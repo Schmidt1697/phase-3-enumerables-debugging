d8d04f5e95c8cda9b66d5984a5bdef4276caf8aa
Anna Schmidt <marshall1697@gmail.com>
2022-10-10 14:26:50 -0600

    commit tree eb41220fe8361b6e4e8de59028d1d498f35a7c00

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,19 @@
 # Write your code here
+# how to return a string with each word reversed
+    # split each word on othe " "
+    # reverse each word and add to new arr
+    # join new arr
+
+require 'pry'
+
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = []
+
+    words.each do |word|
+        reversed_words << word.reverse
+    end
+    reversed_words.join(' ')
+end
+
+puts reverse_each_word("Does it work?")
